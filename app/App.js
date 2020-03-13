15d0cda18a923250644e8d3524a574d5be50f586
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-03-13 11:56:29 -0300

    first commit

---
+++ app/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View >
+        <Text>App</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
