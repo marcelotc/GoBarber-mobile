08dc7e2308679f0b33eaedb9a51cdcdd77f80d38
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-03-15 19:46:32 -0300

    Sign In page

--- app/src/pages/SignIn/index.js
+++ app/src/pages/SignIn/index.js
@@ -1,18 +1,42 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
-import Input from '../../components/Input';
-import Button from '../../components/Button';
+import logo from '../../assets/logo.png';
+
 import Background from '../../components/Background';
+
+import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles';
 
 export default function SignIn() {
     return (
         <Background>
-            <Text>SignIn</Text>
+            <Container>
+                <Image source={logo}></Image>
 
-            <Input style={{ marginTop: 30 }} icon="call" placeholder="Digite seu nome"></Input>
-            <Button>Entrar</Button>
+                <Form>
+                    <FormInput
+                        icon="mail-outline"
+                        keyboardType="email-adress"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Digite seu e-mail"
+                    />
+
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Digite sua senha"
+                    />
+
+                    <SubmitButton onPress={() => { }}> Acessar </SubmitButton>
+                </Form>
+
+                <SignLink onPress={() => { }}>
+                    <SignLinkText>Criar conta gratuita</SignLinkText>
+                </SignLink>
+
+            </Container>
         </Background>
     );
 }
