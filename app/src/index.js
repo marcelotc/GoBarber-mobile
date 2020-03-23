6baee1d0b4ff7de5e490dc4fa3ba28a9b99d551d
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-03-23 17:26:46 -0300

    status bar

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import { StatusBar } from 'react-native';
 import Routes from './routes'
 
 const App = () => {
   return (
-    <Routes></Routes>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1"></StatusBar>
+      <Routes></Routes>
+    </>
   );
 };
 
